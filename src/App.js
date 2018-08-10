d4e054130b3b7f8da53d2262137d54506410f02e
nerdlet <brndnyokabi@gmail.com>
2018-08-10 14:24:12 +0300

    Initial react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Sensors.Africa ! </h1>
+        <p>Welcome to the react set up for sensors.africa</p>
+        <p>Lets get started, shall we?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
