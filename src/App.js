3b8cec780ddfd8b8389706ebd573cb88357cb0a4
nerdlet <brndnyokabi@gmail.com>
2018-08-13 16:37:26 +0300

    Update welcom text

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Sensors.Africa ! </h1>
-        <p>Welcome to the react set up for sensors.africa</p>
+      <div>
+        <h1>Sensors.AFRICA ! </h1>
+        <p>Welcome to the React set up for sensors.AFRICA</p>
         <p>Lets get started, shall we?</p>
       </div>
     );
