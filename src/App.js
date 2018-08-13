3af50e9b9ad626404d4e9f0cb0499a92ebe5fd55
nerdlet <brndnyokabi@gmail.com>
2018-08-13 17:48:08 +0300

    Update changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1> Sensors.Africa!</h1>
-        <p>Welcome to the react set up for sensors.africa</p>
+        <h1>sensors.Africa!</h1>
+        <p>Welcome to the react set up for sensors.AFRICA</p>
         <p>Lets get started, shall we?</p>
       </div>
     );
