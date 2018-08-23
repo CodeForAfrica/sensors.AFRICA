0a25ce7c666f0a41e2038250fef67ab64eb39fd7
nerdlet <brndnyokabi@gmail.com>
2018-08-23 14:59:42 +0300

    Fix routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/assets/css/index.css';
-import Home from 'pages/Home';
+import App from 'App'
+//import Home from 'pages/Home';
 import About from 'pages/About';
 import City from 'pages/City';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
@@ -11,10 +12,10 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Switch>
-          <Route exact path="/" component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/city" component={City}/>
           </Switch>
+          <App />
       </div>
     </BrowserRouter>, 
     document.getElementById('root'));
