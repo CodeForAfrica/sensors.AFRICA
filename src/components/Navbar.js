51588f29e9b582e39a43c86536ee7e092bc309a8
nerdlet <brndnyokabi@gmail.com>
2018-09-03 16:09:04 +0300

    Add menu list and socila media icons on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import{ withStyles } from '@material-ui/core/styles';
 
 const styles = {
-    navbar:{
+    root:{
+        flex:1,
         backgroundColor:'#2e2e2e'
-    } 
+    },
+    toolbar:{
+      paddingRight:'8%',
+      paddingLeft:'8%'
+    },
+    airText:{
+      color:'green'
+    },
+    waterText:{
+      color:'blue'
+    },
+    soundText:{
+      color:'purple'
+    },
+    list:{
+
+    }
 }
 
 class Navbar extends Component {
@@ -19,8 +39,31 @@ class Navbar extends Component {
         const { classes } = this.props;
         return ( 
             <Grid>
-                <AppBar position="static" className={classes.navbar}>
-                    <Toolbar></Toolbar>
+                <AppBar position="static" className={classes.root}>
+                    <Toolbar className={classes.toolbar} item xs={12}>
+                        <List>
+                          <MenuItem className={classes.airText}>AIR</MenuItem>
+                          <MenuItem className={classes.waterText}>WATER</MenuItem>
+                          <MenuItem className={classes.soundText}>SOUND</MenuItem>
+                        </List>
+                        <div>
+                        <FontAwesomeIcon 
+                             className={classes.fa}
+                              icon={['fab', 'facebook-f']} 
+                              size="lg"
+                            />
+                            <FontAwesomeIcon 
+                              className={classes.fa}
+                              icon={['fab', 'twitter']} 
+                              size="lg" 
+                            />
+                            <FontAwesomeIcon
+                              className={classes.fa}
+                              icon={['fab', 'instagram']} 
+                              size="lg"
+                            />
+                        </div>
+                    </Toolbar>
                 </AppBar>
             </Grid>
             
