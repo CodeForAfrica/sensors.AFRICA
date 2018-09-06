b45db4cde42f43817b4f3924fb663d0e101e4be0
David Lemayian <dlemayian@gmail.com>
2018-09-06 19:02:58 +0300

    Update support.js
    Remove unused import.

--- src/components/Support.js
+++ src/components/Support.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
