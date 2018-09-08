a746e1e3a067c79ad9450e2ca6c17bd6b22efa06
nerdlet <brndnyokabi@gmail.com>
2018-09-08 18:05:24 +0300

    Minor updates

--- src/components/IndoorOutdoor.js
+++ src/components/IndoorOutdoor.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import { Card, CardContent, Typography, Button } from '@material-ui/core';
@@ -10,22 +8,13 @@ import OutdoorIcon from '../assets/images/outdooricon.png';
 
 
 const styles = {
-  root:{
-    position:'absolute',
-    display:'center',
-    paddingTop:'3rem',
-    paddingBotton:'3rem',
-    paddingRight:'20%',
-    paddingLeft:'20%',
-    textAlign:'center'
-    
-  },
   indoor:{
-    height:'250px',
-    width:'350px',
+    height:'350px',
+    width:'400px',
     backgroundColor:'#164B3E',
     borderRadius:0,
-    color:'white'
+    color:'white',
+    margin:'2rem'
   },
   indoorButton:{
     borderRadius:0,
@@ -33,11 +22,12 @@ const styles = {
     borderColor:'white'
   },
   outdoor:{
-    height:'250px',
-    width:'350px',
+    height:'350px',
+    width:'400px',
     backgroundColor:'#2FB56B',
     borderRadius:0,
-    color:'white'
+    color:'white',
+    margin:'2rem'
   },
   outdoorButton:{
     borderRadius:0,
@@ -46,6 +36,11 @@ const styles = {
     paddingTop:'0.5rem',
     paddingBottom:'1rem', 
     color:'#F3F3F3'
+  },
+  cardContent:{
+    color:'white',
+    textAlign:'center',
+    paddingTop:'4rem'
   }
 }
 
@@ -53,10 +48,10 @@ class IndoorOutdoor extends Component {
   render() { 
     const { classes } = this.props;
     return (  
-      <Grid container item xs={12} direction="row" justify="space-around" alignItems="center" className={classes.root}>
+      <Grid container direction="row" justify="center" alignItems="center">
           <Card className={classes.indoor}>
-            <CardContent style={{color:'white'}}>
-              <img src={IndoorIcon} Alt="Housing" height="80px"/>
+            <CardContent className={classes.cardContent}>
+              <img src={IndoorIcon} Alt="Housing" height="100px"/>
              <Typography variant="title" style={{color:'white'}}>INDOOR POLLUTION</Typography>
               <Typography variant="caption" className={classes.caption}>Indoor pollution causes an estimated 4.3 million premature deaths each year</Typography>
               <Button variant="outlined" color="upload" className={classes.indoorButton}>READ MORE</Button>
@@ -64,8 +59,8 @@ class IndoorOutdoor extends Component {
           </Card>
       
           <Card className={classes.outdoor} >
-            <CardContent>
-              <img src={OutdoorIcon} Alt="Housing" height="80px"/>
+            <CardContent className={classes.cardContent}>
+              <img src={OutdoorIcon} Alt="Housing" height="100px"/>
               <Typography variant="title" style={{color:'white'}}>OUTDOOR POLLUTION</Typography>
               <Typography variant="caption" className={classes.caption}>Indoor pollution causes an estimated 3 million premature deaths each year</Typography>
              <Button variant="contained" color="upload" className={classes.outdoorButton}>READ MORE</Button>
@@ -74,10 +69,6 @@ class IndoorOutdoor extends Component {
       </Grid>
     );
   }
-}
-
-IndoorOutdoor.Proptypes = {
-  classes:PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(IndoorOutdoor);
