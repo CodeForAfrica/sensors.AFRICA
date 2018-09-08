c6b35d5802a8d22c0b196ce0769fb061f894d991
nerdlet <brndnyokabi@gmail.com>
2018-09-08 18:02:50 +0300

    Minor changes

--- src/components/Issues.js
+++ src/components/Issues.js
@@ -7,18 +7,13 @@ import { withStyles } from '@material-ui/core/styles';
 //import asserts
 import graph from '../../src/assets/images/Graphs.png';
 
-
 const styles = {
     root:{
-        position: 'absolute',
-        display:  'inline-block',
-        height:'auto',
-        minWidth:'100%',
-        textAlign:'center',
-        paddingTop:'3rem',
-        padingBottom:'3rem'
+        flexGrow:1,
+        paddingBottom:'4rem'
     },
     subheading:{
+        paddingTop:'1rem',
         paddingBottom:'0.5rem'
     },
     caption:{
@@ -29,15 +24,15 @@ class Issues extends Component {
     render() { 
         const { classes } = this.props;
         return ( 
-            <Grid className={classes.root} container direction="column" justify="center">
-                <Typography variant="subheading" className={classes.subheading}>THE ISSUES</Typography>
-                <Typography variant="title">Air polution causes 1 in 9 deaths.</Typography>
-                <Typography variant="title">It is the biggest environmental health crisis we face</Typography>
-                <Typography variant="caption" className={classes.caption}>We may not always see it, but air pollution is the cause of some of our most common illnesses</Typography>
-                <Grid>
-                    <img src={graph} alt="Graphs" height="200px"/> 
+                <Grid container direction="column" justify="center" alignItems="center" className={classes.root}>
+                    <Typography variant="subheading" className={classes.subheading}>THE ISSUES</Typography>
+                    <Typography variant="title">Air polution causes 1 in 9 deaths.</Typography>
+                    <Typography variant="title">It is the biggest environmental health crisis we face</Typography>
+                    <Typography variant="caption" className={classes.caption}>We may not always see it, but air pollution is the cause of some of our most common illnesses</Typography>
+                    <Grid>
+                        <img src={graph} alt="Graphs" height="200px"/> 
+                    </Grid>
                 </Grid>
-            </Grid>
          );
     }
 }
