7a1849db5a6a5bc8cba2cce5bcf7b053a6ed888b
nerdlet <brndnyokabi@gmail.com>
2018-09-08 18:07:42 +0300

    Update minor changes

--- src/pages/air/AirLanding.js
+++ src/pages/air/AirLanding.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
+import Grid from '@material-ui/core/Grid';
+
 import Issues from '../../components/Issues';
 import IndoorOutdoor from '../../components/IndoorOutdoor';
 
 class AirLanding extends Component {
     render() { 
         return ( 
-            <React.Fragment>
+            <Grid container item xs={12}>
                 <Issues />
                 <IndoorOutdoor />
-            </React.Fragment>
+            </Grid>
          );
     }
 }
