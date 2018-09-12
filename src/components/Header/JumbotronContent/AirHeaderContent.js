328f3fe92727e785617366e6a2f1874b76f2a0d3
nerdlet <brndnyokabi@gmail.com>
2018-09-12 12:49:38 +0300

    Use jsx for the header texts display

--- src/components/Header/JumbotronContent/AirHeaderContent.js
+++ src/components/Header/JumbotronContent/AirHeaderContent.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
-    titleSection: {
-        textAlign: 'center',
-        paddingTop: '2rem',
-        paddingRight: '25%',
-        paddingLeft: '25%'
-    },
-    menuButton: {
-        color: 'white'
-    }
-}
+  titleSection: {
+    textAlign: "center",
+    paddingTop: "2rem",
+    paddingRight: "25%",
+    paddingLeft: "25%"
+  },
+  menuButton: {
+    color: "white"
+  }
+};
 
 class AirHeaderContent extends Component {
-    render() {
-        const { classes } = this.props;
-        return (
-            <Grid className={classes.titleSection}>
-                <Typography variant="display2" style={{ color: 'white' }}>WE HAVE TESTED THE QUALITY OF YOUR CITY'S AIR</Typography>
-            </Grid>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid className={classes.titleSection}>
+        <Typography variant="display2" style={{ color: "white" }}>
+          {this.props.title}
+        </Typography>
+      </Grid>
+    );
+  }
 }
 
 AirHeaderContent.PropTypes = {
-    classes: PropTypes.object.isRequired,
-}
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(AirHeaderContent);
--- src/components/Header/JumbotronContent/JoinHeaderContent.js
+++ src/components/Header/JumbotronContent/JoinHeaderContent.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
-    titleSection: {
-        textAlign: 'center',
-        paddingTop: '2rem',
-        paddingRight: '25%',
-        paddingLeft: '25%'
-    },
-    headerText: {
-        color: 'white',
-        paddingBottom: '1rem'
-    }
-}
+  titleSection: {
+    textAlign: "center",
+    paddingTop: "2rem",
+    paddingRight: "25%",
+    paddingLeft: "25%"
+  },
+  headerText: {
+    color: "white",
+    paddingBottom: "1rem"
+  }
+};
 
 class JoinNetworkContent extends Component {
-    render() {
-        const { classes } = this.props;
-        return (
-            <Grid className={classes.titleSection}>
-                <Typography variant="display2" className={classes.headerText}>JOIN THE NETWORK</Typography>
-                <Typography variant="subheading" className={classes.headerText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nam ut purus quis massa interdum egestas quis id velit. Proin non nisi porta sem placerat blandit in eget turpis.
-                 Quisque faucibus auctor ligula et posuere. Vestibulum ultrices lobortis turpis eu vulputate.
-                 Fusce eget erat libero.</Typography>
-            </Grid>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid className={classes.titleSection}>
+        <Typography variant="display2" className={classes.headerText}>
+          {this.props.title}
+        </Typography>
+        <Typography variant="subheading" className={classes.headerText}>
+          {this.props.subheading}
+        </Typography>
+      </Grid>
+    );
+  }
 }
 
 JoinNetworkContent.PropTypes = {
-    classes: PropTypes.object.isRequired,
-}
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(JoinNetworkContent);
