fe92be2214168569f7a259ecfcf4b92e64c53c70
nerdlet <brndnyokabi@gmail.com>
2018-09-19 11:38:15 +0300

    Add statistics component

--- src/components/CityComponents/StatisticsReport.js
+++ src/components/CityComponents/StatisticsReport.js
@@ -6,18 +6,29 @@ import { withStyles } from "@material-ui/core/styles";
 const styles = {
   root: {
     paddingTop: "2rem",
-    paddingBottom: "2rem"
+    paddingBottom: "2rem",
+    paddingLeft: "3rem",
+    paddingRight: "3rem"
   },
   centerGrid: {
     borderRight: "1px solid grey",
     borderLeft: "1px solid grey",
-    height: "200px"
+    height: "250px",
+    paddingTop: "2rem",
+    paddingLeft: "2rem",
+    paddingRight: "2rem",
+    textAlign: "center"
   },
   grid: {
-    height: "250px"
+    height: "250px",
+    textAlign: "center",
+    paddingRight: "2rem",
+    paddingLeft: "2rem",
+    paddingTop: "2rem"
   },
   highlight: {
-    color: "#2FB56B"
+    color: "#2FB56B",
+    fontWeight: "500"
   }
 };
 
@@ -43,18 +54,26 @@ class StatisticReport extends Component {
           alignItems="center"
           className={classes.grid}
         >
-          <Typography variant="subheading">
-            Death by air pollution in kenya yearly
-          </Typography>
-          <Typography variant="display2" className={classes.highlight}>
-            5,102
-          </Typography>
-          <Typography variant="subheading">
-            Child Deaths caused by air pollution inkenya yearly
-          </Typography>
-          <Typography variant="display2" className={classes.highlight}>
-            2,144
-          </Typography>
+          <div
+            style={{
+              paddingBottom: "1.5rem"
+            }}
+          >
+            <Typography variant="subheading">
+              Death by air pollution in kenya yearly
+            </Typography>
+            <Typography variant="display2" className={classes.highlight}>
+              5,102
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="subheading">
+              Child Deaths caused by air pollution in kenya yearly
+            </Typography>
+            <Typography variant="display2" className={classes.highlight}>
+              2,144
+            </Typography>
+          </div>
         </Grid>
 
         <Grid
@@ -66,14 +85,8 @@ class StatisticReport extends Component {
           alignItems="center"
           className={classes.centerGrid}
         >
-          <div
-            style={{
-              textALign: "center",
-              paddingRight: "1rem",
-              paddingLeft: "1rem"
-            }}
-          >
-            <Typography variant="subheading">
+          <div>
+            <Typography variant="subheading" style={{ paddingBottom: "1rem" }}>
               The top illness caused by air pollution in kenya is
             </Typography>
             <Typography variant="display1" className={classes.highlight}>
@@ -91,16 +104,27 @@ class StatisticReport extends Component {
           alignItems="center"
           className={classes.grid}
         >
-          <Typography variant="title">POLLUTION LEVELS IN NAIROBI </Typography>
-          <Typography variant="subheading">
-            The air in Nairobi has an annual average of
-          </Typography>
-          <Typography variant="display1" className={classes.highlight}>
-            17 ug/m3
-          </Typography>
-          <Typography variant="subheading">
-            of PM2.5 particles. That is 70% morre than the WHO safe level.{" "}
-          </Typography>
+          <div style={{ paddingRight: "2rem", paddingLeft: "2rem" }}>
+            <Typography variant="subheading">
+              <b>POLLUTION LEVELS IN NAIROBI</b>
+            </Typography>
+            <Typography
+              variant="subheading"
+              style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+            >
+              The air in Nairobi has an annual average of
+            </Typography>
+            <Typography
+              variant="display1"
+              className={classes.highlight}
+              style={{ paddingBottom: "1rem" }}
+            >
+              17 ug/m3
+            </Typography>
+            <Typography variant="subheading">
+              of PM2.5 particles. That is 70% more than the WHO safe level.
+            </Typography>
+          </div>
         </Grid>
       </Grid>
     );
