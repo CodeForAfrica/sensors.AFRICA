09f6f03055773a76534b3c83a32c6837a4bf16f9
nerdlet <brndnyokabi@gmail.com>
2018-09-22 16:25:51 +0300

    Add working hambuger menu with anchor links

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -11,7 +11,7 @@ import AboutHeaderContent from "../Header/JumbotronContent/AboutHeaderContent.js
 
 const styles = {
   jumbotron: {
-    backgroundColor: "#454545",
+    backgroundColor: "#2FB56B",
     height: "400px",
     borderRadius: "none"
   }
---
+++ src/pages/air/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+//import components
+import Stories from "../../components/About/Stories";
+import Navbar from "../../components/Header/Navbar";
+import AboutHeader from "../../components/About/AboutHeader";
+import Partners from "../../components/About/Partners";
+import Staff from "../../components/About/Staff";
+import Footer from "../../components/Footer";
+import Support from "../../components/Support";
+
+class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <AboutHeader />
+        <Partners />
+        <Staff />
+        <Stories />
+        <Support />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default About;
