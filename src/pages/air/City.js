a293e3a46a4ac318c341eb0718bf32bfd4f2c859
_ Kilemensi <kilemensi@gmail.com>
2018-10-10 12:15:39 +0300

    Fix air city page width

--- src/pages/air/City.js
+++ src/pages/air/City.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
 import Navbar from '../../components/Header/Navbar';
 import Footer from '../../components/Footer';
@@ -8,24 +10,90 @@ import KenyaMap from '../../components/Maps/KenyaMap';
 import Neighbourhood from '../../components/CityComponents/Neighbourhood';
 import CityHeader from '../../components/CityComponents/Header/CityHeader';
 import CallToAction from '../../components/CityComponents/CallToAction';
-import HostSensors from '../../components/CityComponents/HostSensors';
+import HostSensor from '../../components/CityComponents/HostSensors';
 import QualityStats from '../../components/CityComponents/SensorsQualityStats/QualityStats';
 import PollutionStats from '../../components/CityComponents/PollutionStats';
 
-function City() {
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  contained: {
+    width: 'auto',
+    margin: '0 auto',
+    [theme.breakpoints.up('md')]: {
+      width: '970px'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '1170px'
+    }
+  },
+  separator: {
+    color: 'rgba(0, 0, 0, 0.2)'
+  }
+});
+function City({ classes }) {
   return (
-    <Grid>
-      <Navbar />
-      <CityHeader />
-      <HostSensors />
-      <PollutionStats />
-      <Neighbourhood />
-      <KenyaMap />
-      <QualityStats />
-      <CallToAction />
-      <Footer />
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <Navbar />
+      </Grid>
+      <Grid item xs={12}>
+        <CityHeader />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid
+          container
+          className={classes.contained}
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <HostSensor />
+          </Grid>
+          <Grid item xs={12}>
+            <hr className={classes.separator} />
+          </Grid>
+          <Grid item xs={12}>
+            <PollutionStats />
+          </Grid>
+          <Grid item xs={12}>
+            <Neighbourhood />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <KenyaMap />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid
+          container
+          className={classes.contained}
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <QualityStats />
+          </Grid>
+          <Grid item xs={12}>
+            <CallToAction />
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Footer />
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
 
-export default City;
+City.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(City);
