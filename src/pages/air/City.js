7f75cfaccd95edcd5055828f9da1874e74e00c81
_ Kilemensi <kilemensi@gmail.com>
2018-10-17 13:06:20 +0300

    Format City.js

--- src/pages/air/City.js
+++ src/pages/air/City.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
 
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
@@ -38,102 +38,136 @@ const styles = theme => ({
 
 const DEFAULT_CITY = { value: 'nairobi', label: 'Nairobi, Kenya' };
 const CITIES_LOCATION = {
-  'nairobi': {
-    latitude: "-1.",
-    longitude: "36.",
-    label: "Nairobi, Kenya"
+  nairobi: {
+    latitude: '-1.',
+    longitude: '36.',
+    label: 'Nairobi, Kenya'
   },
-  'lagos': {
-    latitude: "6.",
-    longitude: "3.",
-    label: "Lagos, Nigeria"
+  lagos: {
+    latitude: '6.',
+    longitude: '3.',
+    label: 'Lagos, Nigeria'
   },
   'dar-es-salaam': {
-    latitude: "-6.",
-    longitude: "39.",
-    label: "Dar-es-salaam, Tanzania"
+    latitude: '-6.',
+    longitude: '39.',
+    label: 'Dar-es-salaam, Tanzania'
   }
-}
+};
 class City extends Component {
   constructor() {
     super();
     this.state = {
-      city: DEFAULT_CITY,
-    }
-   this.handleChange = this.handleChange.bind(this);
+      city: DEFAULT_CITY
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    const city = this.props.location.state.city || DEFAULT_CITY;
     fetch('http://api.airquality.codeforafrica.org/v1/now/')
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      let cells = data.filter((sensor) =>
-          sensor.location.latitude.startsWith(CITIES_LOCATION[city.value].latitude) &&
-          sensor.location.longitude.startsWith(CITIES_LOCATION[city.value].longitude) && (
-          (sensor.sensor.sensor_type.name === "SDS021" && sensor.sensordatavalues.length >= 2) ||
-          (sensor.sensor.sensor_type.name === 'SDS011' && sensor.sensordatavalues.length >= 2))
-        ).reduce((sensorGroup, {sensor, sensordatavalues}) => {
-          if (!sensorGroup[sensor.id]) sensorGroup[sensor.id] = [];
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        let cells = data
+          .filter(
+            sensor =>
+              sensor.location.latitude.startsWith(
+                CITIES_LOCATION[city.value].latitude
+              ) &&
+              sensor.location.longitude.startsWith(
+                CITIES_LOCATION[city.value].longitude
+              ) &&
+              ((sensor.sensor.sensor_type.name === 'SDS021' &&
+                sensor.sensordatavalues.length >= 2) ||
+                (sensor.sensor.sensor_type.name === 'SDS011' &&
+                  sensor.sensordatavalues.length >= 2))
+          )
+          .reduce((sensorGroup, { sensor, sensordatavalues }) => {
+            if (!sensorGroup[sensor.id]) sensorGroup[sensor.id] = [];
             sensordatavalues.map(val => {
-              if(val.value_type === "P2") {
+              if (val.value_type === 'P2') {
                 sensorGroup[sensor.id].push(parseFloat(val.value));
               }
             });
-            return sensorGroup
+            return sensorGroup;
           }, {});
-      return Promise.resolve(cells);
-    }).then((cells) => {
-          for (const [key, value] of Object.entries(cells)) {
-             cells[key] = ((cells[key].reduce((a,b) => a+b))/(cells[key]).length).toFixed(2);
-           }
-          return cells;
-    }).then(value => {
-      this.setState((state) => {
-        return { city: city,
-                 cityAirPol: (((Object.values(value)).reduce((a, b) =>
-          { return parseFloat(a) + parseFloat(b) }, 0))/(Object.values(value)).length).toFixed(2)
+        return Promise.resolve(cells);
+      })
+      .then(cells => {
+        for (const [key, value] of Object.entries(cells)) {
+          cells[key] = (
+            cells[key].reduce((a, b) => a + b) / cells[key].length
+          ).toFixed(2);
         }
+        return cells;
+      })
+      .then(value => {
+        this.setState(state => {
+          return {
+            city: city,
+            cityAirPol: (
+              Object.values(value).reduce((a, b) => {
+                return parseFloat(a) + parseFloat(b);
+              }, 0) / Object.values(value).length
+            ).toFixed(2)
+          };
+        });
       });
-    });
-
   }
 
   handleChange(option) {
     const city = option || DEFAULT_CITY;
     fetch('http://api.airquality.codeforafrica.org/v1/now/')
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      let cells = data.filter((sensor) =>
-          sensor.location.latitude.startsWith(CITIES_LOCATION[city.value].latitude) &&
-          sensor.location.longitude.startsWith(CITIES_LOCATION[city.value].longitude) && (
-          (sensor.sensor.sensor_type.name === "SDS021" && sensor.sensordatavalues.length >= 2) ||
-          (sensor.sensor.sensor_type.name === 'SDS011' && sensor.sensordatavalues.length >= 2))
-        ).reduce((sensorGroup, {sensor, sensordatavalues}) => {
-          if (!sensorGroup[sensor.id]) sensorGroup[sensor.id] = [];
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        let cells = data
+          .filter(
+            sensor =>
+              sensor.location.latitude.startsWith(
+                CITIES_LOCATION[city.value].latitude
+              ) &&
+              sensor.location.longitude.startsWith(
+                CITIES_LOCATION[city.value].longitude
+              ) &&
+              ((sensor.sensor.sensor_type.name === 'SDS021' &&
+                sensor.sensordatavalues.length >= 2) ||
+                (sensor.sensor.sensor_type.name === 'SDS011' &&
+                  sensor.sensordatavalues.length >= 2))
+          )
+          .reduce((sensorGroup, { sensor, sensordatavalues }) => {
+            if (!sensorGroup[sensor.id]) sensorGroup[sensor.id] = [];
             sensordatavalues.map(val => {
-              if(val.value_type === "P2") {
+              if (val.value_type === 'P2') {
                 sensorGroup[sensor.id].push(parseFloat(val.value));
               }
             });
-            return sensorGroup
+            return sensorGroup;
           }, {});
-      return Promise.resolve(cells);
-    }).then((cells) => {
-          for (const [key, value] of Object.entries(cells)) {
-             cells[key] = ((cells[key].reduce((a,b) => a+b))/(cells[key]).length).toFixed(2);
-           }
-          return cells;
-    }).then(value => {
-      this.setState((state) => {
-        return { city: city,
-                 cityAirPol: (((Object.values(value)).reduce((a, b) =>
-          { return parseFloat(a) + parseFloat(b) }, 0))/(Object.values(value)).length).toFixed(2)
+        return Promise.resolve(cells);
+      })
+      .then(cells => {
+        for (const [key, value] of Object.entries(cells)) {
+          cells[key] = (
+            cells[key].reduce((a, b) => a + b) / cells[key].length
+          ).toFixed(2);
         }
+        return cells;
+      })
+      .then(value => {
+        this.setState(state => {
+          return {
+            city: city,
+            cityAirPol: (
+              Object.values(value).reduce((a, b) => {
+                return parseFloat(a) + parseFloat(b);
+              }, 0) / Object.values(value).length
+            ).toFixed(2)
+          };
+        });
       });
-    });
   }
 
   render() {
