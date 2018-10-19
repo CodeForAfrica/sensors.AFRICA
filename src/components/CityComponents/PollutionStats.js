677d6d8d031beb70934f873f8b530de24ffa42c8
nerdlet <brndnyokabi@gmail.com>
2018-10-19 18:19:57 +0300

    Improve styling

--- src/components/CityComponents/PollutionStats.js
+++ src/components/CityComponents/PollutionStats.js
@@ -12,7 +12,6 @@ const styles = theme => ({
   },
   mainGrid: {
     flexGrow: 1,
-    marginTop: theme.spacing.unit * 8,
     marginBottom: theme.spacing.unit * 4,
     paddingRight: '8rem',
     paddingLeft: '8rem'
@@ -33,20 +32,33 @@ const styles = theme => ({
     }
   },
   stat: {
-    textAlign: 'center'
+    textAlign: 'center',
+    paddingTop: '1rem'
   },
   centerStat: {
     textAlign: 'center',
+    paddingTop: '1rem',
     [theme.breakpoints.up('md')]: {
       paddingtop: '0.5rem'
     }
   },
+  centerStatInnerGrid: {
+    paddingTop: 0,
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem',
+    paddingBottom: '2rem'
+  },
   statHighlight: {
     color: theme.palette.primary.light,
-    fontWeight: theme.typography.h6.fontWeight
+    fontWeight: theme.typography.h6.fontWeight,
+    paddingBottom: '1rem'
   },
   subtitle1: {
     fontWeight: theme.typography.h6.fontWeight
+  },
+  title: {
+    color: theme.typography.h6.color,
+    paddingTop: '3rem'
   }
 });
 
@@ -73,8 +85,8 @@ function PollutionStats({ classes }) {
           justify="center"
           alignItems="center"
         >
-          <Grid item xs={8} className={classes.stat}>
-            <Grid style={{ paddingTop: '2rem' }}>
+          <Grid item xs={10} className={classes.stat}>
+            <Grid style={{ paddingTop: '4rem' }}>
               <Typography variant="subtitle1" className={classes.subtitle1}>
                 Death by air pollution in Kenya yearly
               </Typography>
@@ -83,9 +95,17 @@ function PollutionStats({ classes }) {
               </Typography>
             </Grid>
 
-            <Grid style={{ marginTop: '2rem', paddingBottom: '2rem' }}>
-              <Typography variant="subtitle1" className={classes.subtitle1}>
-                Child Deaths caused by air pollution in kenya yearly
+            <Grid style={{ paddingTop: '1rem', paddingBottom: '2rem' }}>
+              <Typography
+                variant="subtitle1"
+                style={{
+                  fontWight: 500,
+                  paddingRight: '2.5rem',
+                  paddingLeft: '2.5rem'
+                }}
+              >
+                Child Deaths
+                <b /> caused by air pollution in Kenya yearly
               </Typography>
               <Typography variant="h3" className={classes.statHighlight}>
                 2,144
@@ -103,10 +123,9 @@ function PollutionStats({ classes }) {
           alignItems="center"
           className={classes.statGrid}
         >
-          <Grid item xs={8} className={classes.centerStat}>
+          <Grid item xs={10} className={classes.centerStat}>
             <Grid
               style={{
-                paddingTop: 0,
                 paddingLeft: '0.5rem',
                 paddingRight: '0.5rem',
                 paddingBottom: '2rem'
@@ -132,27 +151,28 @@ function PollutionStats({ classes }) {
           justify="center"
           alignItems="center"
         >
-          <Grid item xs={8} className={classes.stat}>
-            <Typography variant="caption">
+          <Grid item xs={10} className={classes.stat}>
+            <Typography variant="heading" className={classes.title}>
               <b>POLLUTION LEVELS IN NAIROBI</b>
             </Typography>
-            <Typography
-              variant="subtitle1"
-              className={classes.subtitle1}
-              style={{ paddingTop: '1rem', paddingBottom: '1rem' }}
-            >
-              The air in Nairobi has an annual average of
-            </Typography>
-            <Typography
-              variant="h4"
-              className={classes.statHighlight}
-              style={{ paddingBottom: '1rem' }}
-            >
-              17 ug/m3
-            </Typography>
-            <Typography variant="subtitle1" className={classes.subtitle1}>
-              of PM2.5 particles. That is 70% more than the WHO safe level.
-            </Typography>
+            <Grid>
+              <Typography
+                variant="subtitle1"
+                style={{ paddingTop: '1rem', fontWeight: 500 }}
+              >
+                The air in Nairobi has an annual average of
+              </Typography>
+            </Grid>
+            <Grid style={{ margin: '1rem' }}>
+              <Typography variant="h4" className={classes.statHighlight}>
+                17 <sup style={{ fontSize: '16px' }}>ug/m3</sup>
+              </Typography>
+            </Grid>
+            <Grid style={{ paddingRight: '2.5rem', paddingLeft: '2.5rem' }}>
+              <Typography variant="subtitle1" className={classes.subtitle1}>
+                of PM2.5 particles. That is 70% more than the WHO safe level.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
