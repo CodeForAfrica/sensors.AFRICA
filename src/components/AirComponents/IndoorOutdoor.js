3ff1f1a4a70cbf04339c9e6e2e8b28fc90b3d3cb
_ Kilemensi <kilemensi@gmail.com>
2018-10-23 10:53:31 +0300

    Make it mobile responsive

--- src/components/AirComponents/IndoorOutdoor.js
+++ src/components/AirComponents/IndoorOutdoor.js
@@ -9,114 +9,153 @@ import OutdoorIcon from '../../assets/images/outdooricon.png';
 
 const styles = theme => ({
   root: {
-    paddingBottom: '4rem',
-    backgroundColor: 'white'
-  },
-  mainGrid: {
-    paddingRight: '8rem',
-    paddingLeft: '8rem'
+    backgroundColor: 'white',
+    [theme.breakpoints.up('md')]: {
+      paddingBottom: '3rem'
+    }
   },
   indoor: {
     backgroundColor: theme.palette.primary.dark,
-    borderRadius: 0,
     color: 'white',
-    margin: '2rem'
+    width: '100vw',
+    [theme.breakpoints.up('md')]: {
+      width: '25rem',
+      height: '18.125rem',
+      marginRight: '4.8125rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '37.875rem',
+      height: '29.75rem',
+      marginRight: '1.875rem',
+      padding: '3rem'
+    }
   },
   indoorButton: {
-    borderRadius: 0,
     color: '#F3F3F3',
     borderColor: 'white',
-    fontWeight: 'bolder'
+    fontWeight: 800,
+    fontSize: theme.typography.subtitle1.fontSize,
+    [theme.breakpoints.up('lg')]: {
+      height: '3.5rem',
+      paddingLeft: '2rem',
+      paddingRight: '2rem'
+    }
   },
   outdoor: {
     backgroundColor: theme.palette.primary.light,
-    borderRadius: 0,
     color: 'white',
-    margin: '2rem'
+    width: '100vw',
+    [theme.breakpoints.up('md')]: {
+      width: '25rem',
+      height: '18.125rem',
+      marginLeft: '4.8125rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '37.875rem',
+      height: '29.75rem',
+      marginLeft: '1.875rem',
+      padding: '3rem'
+    }
   },
   outdoorButton: {
-    borderRadius: 0,
-    fontWeight: 'bolder',
-    color: theme.palette.primary.dark
+    fontWeight: 800,
+    fontSize: theme.typography.subtitle1.fontSize,
+    color: theme.palette.primary.dark,
+    [theme.breakpoints.up('lg')]: {
+      height: '3.5rem',
+      paddingLeft: '2rem',
+      paddingRight: '2rem'
+    }
   },
   buttonLink: {
     textDecoration: 'none'
   },
+  title: {
+    color: 'white',
+    [theme.breakpoints.up('lg')]: {
+      paddingTop: '1.5rem',
+      fontSize: theme.typography.h4.fontSize,
+      fontWeight: theme.typography.h4.fontWeight
+    }
+  },
   caption: {
-    paddingTop: '0.5rem',
+    color: '#F3F3F3',
     paddingBottom: '1rem',
-    color: '#F3F3F3'
+    [theme.breakpoints.up('lg')]: {
+      fontSize: theme.typography.subtitle1.fontSize,
+      fontWeight: theme.typography.subtitle1.fontWeight,
+      paddingTop: '2rem'
+    }
   },
   cardContent: {
     color: 'white',
-    textAlign: 'center',
-    paddingTop: '4rem'
+    textAlign: 'center'
   },
   img: {
-    height: '100%',
-    width: 'auto'
+    height: '5rem',
+    width: 'auto',
+    [theme.breakpoints.up('lg')]: {
+      height: '100%'
+    }
   }
 });
 
 function IndoorOutdoor({ classes }) {
   return (
-    <Grid item xs={12} className={classes.root}>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        className={classes.mainGrid}
-      >
-        <Grid item xs={6}>
-          <Card className={classes.indoor}>
-            <CardContent className={classes.cardContent}>
-              <img src={IndoorIcon} Alt="Housing" className={classes.img} />
-              <Typography variant="h5" style={{ color: 'white' }}>
-                INDOOR POLLUTION
-              </Typography>
-              <Typography variant="caption" className={classes.caption}>
-                Indoor pollution causes an estimated 4.3 million premature
-                deaths each year.
-              </Typography>
-              <a
-                href="https://medium.com/code-for-africa/tagged/innovateafrica"
-                className={classes.buttonLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="outlined" className={classes.indoorButton}>
-                  READ MORE
-                </Button>
-              </a>
-            </CardContent>
-          </Card>
-        </Grid>
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item>
+        <Card className={classes.indoor}>
+          <CardContent className={classes.cardContent}>
+            <img src={IndoorIcon} Alt="Housing" className={classes.img} />
+            <Typography variant="h5" className={classes.title}>
+              INDOOR POLLUTION
+            </Typography>
+            <Typography variant="subtitle2" className={classes.caption}>
+              Indoor pollution causes an estimated 4.3 million premature deaths
+              each year.
+            </Typography>
+            <a
+              href="https://medium.com/code-for-africa/tagged/innovateafrica"
+              className={classes.buttonLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="outlined" className={classes.indoorButton}>
+                READ MORE
+              </Button>
+            </a>
+          </CardContent>
+        </Card>
+      </Grid>
 
-        <Grid item xs={6}>
-          <Card className={classes.outdoor}>
-            <CardContent className={classes.cardContent}>
-              <img src={OutdoorIcon} Alt="Housing" className={classes.img} />
-              <Typography variant="h5" style={{ color: 'white' }}>
-                OUTDOOR POLLUTION
-              </Typography>
-              <Typography variant="caption" className={classes.caption}>
-                Outdoor pollution causes an estimated 4.2 million premature
-                deaths each year.
-              </Typography>
-              <a
-                href="https://medium.com/code-for-africa/tagged/innovateafrica"
-                className={classes.buttonLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button variant="contained" className={classes.outdoorButton}>
-                  READ MORE
-                </Button>
-              </a>
-            </CardContent>
-          </Card>
-        </Grid>
+      <Grid item>
+        <Card className={classes.outdoor}>
+          <CardContent className={classes.cardContent}>
+            <img src={OutdoorIcon} alt="Housing" className={classes.img} />
+            <Typography variant="h5" className={classes.title}>
+              OUTDOOR POLLUTION
+            </Typography>
+            <Typography variant="subtitle2" className={classes.caption}>
+              Outdoor pollution causes an estimated 4.2 million premature deaths
+              each year.
+            </Typography>
+            <a
+              href="https://medium.com/code-for-africa/tagged/innovateafrica"
+              className={classes.buttonLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button variant="contained" className={classes.outdoorButton}>
+                READ MORE
+              </Button>
+            </a>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
