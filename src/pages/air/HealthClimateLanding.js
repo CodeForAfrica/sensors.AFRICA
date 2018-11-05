592d626a06aef3838f4b287e927151769997e111
nerdlet <brndnyokabi@gmail.com>
2018-11-05 16:49:41 +0300

    Add header jumbotron component

--- src/pages/air/HealthClimateLanding.js
+++ src/pages/air/HealthClimateLanding.js
@@ -3,11 +3,13 @@ import { Grid } from '@material-ui/core';
 import Navbar from '../../components/Header/Navbar';
 import Footer from '../../components/Footer';
 import SourcesInfo from '../../components/HealthClimateComponents/SourcesInfo';
+import HealthClimateHeader from '../../components/HealthClimateComponents/HealthClimateHeader';
 
 function HealthClimateLanding() {
   return (
     <Grid>
       <Navbar />
+      <HealthClimateHeader />
       <SourcesInfo />
       <Footer />
     </Grid>
