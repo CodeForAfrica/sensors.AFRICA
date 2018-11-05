00af9c0509184e88e129d21f57eb2ee33a19cd45
nerdlet <brndnyokabi@gmail.com>
2018-11-05 16:08:06 +0300

    Add image icons

--- src/pages/air/HealthClimateLanding.js
+++ src/pages/air/HealthClimateLanding.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
 import Navbar from '../../components/Header/Navbar';
 import Footer from '../../components/Footer';
 import SourcesInfo from '../../components/HealthClimateComponents/SourcesInfo';
+import HealthBurdenComponent from '../../components/HealthClimateComponents/HealthBurden';
 
 function HealthClimateLanding() {
   return (
     <Grid>
       <Navbar />
       <SourcesInfo />
+      <HealthBurdenComponent />
       <Footer />
     </Grid>
   );
