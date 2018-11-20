676b0bb91861b09210080e12972368e7b2fd2b0d
nerdlet <brndnyokabi@gmail.com>
2018-11-20 09:51:05 +0300

    Add addson to ./stories

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,7 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-options/register';
+import '@storybook/addon-viewport/register';
+import 'storybook-readme/register';
+import 'storybook-addon-material-ui/register';
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-import { Button, Welcome } from '@storybook/react/demo';
+const Theme = createMuiTheme({
+  palette: {
+    primary: { main: '#1a995b', light: '#2FB56B', dark: '#164B3E' },
+    secondary: { main: '#424143', dark: '#2A2A2B' }
+  },
+  themeName: 'Sensors.AFRICA Light Theme'
+});
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Material Custom Theme', module).addDecorator(muiTheme([Theme]));
