5ff6730592f974f8bb6c506bb16811979c3ca3d8
nerdlet <brndnyokabi@gmail.com>
2018-11-20 17:04:44 +0300

    Add manger app content

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,27 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withOptions } from '@storybook/addon-options';
 
 function loadStories() {
   require('../src/stories');
 }
 
+const storyWrapper = story => <div>{story()}</div>;
+addDecorator(
+  withInfo({
+    inline: 'true',
+    header: 'true',
+    source: 'true',
+    maxPropsToLine: '1'
+  })
+);
+addDecorator(
+  withOptions({
+    name: 'Sensors.Africa',
+    url: 'https://sensors.africa/'
+  })
+);
+
+addDecorator(storyWrapper);
 configure(loadStories, module);
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { storiesOf, addDecorator } from '@storybook/react';
+import { muiTheme } from 'storybook-addon-material-ui';
 
-const Theme = createMuiTheme({
+//Example material ui
+import Card from '@material-ui/core/Card';
+
+storiesOf('sensors.AFRICA', module)
+  .addDecorator(muiTheme())
+  .add('Introduction', () => <Card />)
+  .add('ReadMe', () => <Card />);
+
+storiesOf('UI.all', module)
+  .addDecorator(muiTheme())
+  .add('Theme', () => <Card />)
+  .add('Typography', () => <Card />);
+
+storiesOf('Components', module)
+  .addDecorator(muiTheme())
+  .add('Buttons', () => <Card />)
+  .add('Cards', () => <Card />)
+  .add('Navbar', () => <Card />)
+  .add('Menubar', () => <Card />)
+  .add('Header Jumbotron', () => <Card />)
+  .add('Hambuger Section', () => <Card />)
+  .add('Button', () => <Card />)
+  .add('Graphs', () => <Card />)
+  .add('Footer', () => <Card />);
+
+storiesOf('API', module)
+  .addDecorator(muiTheme())
+  .add('API introduction', () => <Card />);
+
+/*const THEME = createMuiTheme({
   palette: {
     primary: { main: '#1a995b', light: '#2FB56B', dark: '#164B3E' },
     secondary: { main: '#424143', dark: '#2A2A2B' }
   },
-  themeName: 'Sensors.AFRICA Light Theme'
+  themeName: 'Custom Light Theme'
 });
 
-storiesOf('Material Custom Theme', module).addDecorator(muiTheme([Theme]));
+storiesOf('Material Custom Theme', module).addDecorator(muiTheme([THEME]));*/
