ec8ead8e3b2bcfbecb6da29e270529daf8e0efe9
nerdlet <brndnyokabi@gmail.com>
2018-11-23 11:17:19 +0300

    Update changes

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,30 +5,43 @@ import { muiTheme } from 'storybook-addon-material-ui';
 //Example material ui
 import Card from '@material-ui/core/Card';
 
+const Theme = {
+  themeName: 'Sensors.Africa theme',
+  palette: {
+    primary1Color: '#00bcd4',
+    alternateTextColor: '#4a4a4a',
+    canvasColor: '#616161',
+    textColor: '#bdbdbd',
+    secondaryTextColor: 'rgba(255, 255, 255, 0.54)',
+    disabledColor: '#757575',
+    accent1Color: '#607d8b'
+  }
+};
+
 storiesOf('sensors.AFRICA', module)
-  .addDecorator(muiTheme())
+  .addDecorator(muiTheme([Theme]))
   .add('Introduction', () => <Card />)
   .add('ReadMe', () => <Card />);
 
 storiesOf('UI.all', module)
-  .addDecorator(muiTheme())
+  .addDecorator(muiTheme([Theme]))
   .add('Theme', () => <Card />)
   .add('Typography', () => <Card />);
 
 storiesOf('Components', module)
-  .addDecorator(muiTheme())
+  .addDecorator(muiTheme([Theme]))
   .add('Buttons', () => <Card />)
   .add('Cards', () => <Card />)
   .add('Navbar', () => <Card />)
   .add('Menubar', () => <Card />)
-  .add('Header Jumbotron', () => <Card />)
+  .add('Jumbotron', () => <Card />)
   .add('Hambuger Section', () => <Card />)
   .add('Button', () => <Card />)
   .add('Graphs', () => <Card />)
   .add('Footer', () => <Card />);
 
 storiesOf('API', module)
-  .addDecorator(muiTheme())
+  .addDecorator(muiTheme([Theme]))
   .add('API introduction', () => <Card />);
 
 //Define MATERIAL  theme
