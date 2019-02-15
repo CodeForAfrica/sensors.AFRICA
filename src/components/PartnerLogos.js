1346d3c54da81d28c1916f61047bae9063bf6224
nerdlet <brndnyokabi@gmail.com>
2019-02-15 20:11:22 +0300

    Update changes

--- src/components/PartnerLogos.js
+++ src/components/PartnerLogos.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Grid, Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 import worldbankgroup from '../assets/images/partners/worldbankgroup.png';
 import liquidtelcom from '../assets/images/partners/liquidtelcom.png';
-import data4sdg from '../assets/images/partners/data4sdg.jpg';
+import data4sdg from '../assets/images/partners/partnershipsdg.png';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    paddingTop: '3rem',
-    paddingBottom: '3rem',
+    paddingTop: '2rem',
+    paddingBottom: '2rem',
     backgroundColor: 'white'
   },
   mainGrid: {
-    margin: '2rem',
+    margin: '1rem',
     width: 'auto'
   },
   img: {
@@ -37,7 +37,7 @@ const styles = theme => ({
   partnerGrid: {
     paddingTop: 0,
     [theme.breakpoints.down('sm')]: {
-      paddingTop: '2rem',
+      paddingTop: '1rem',
       textAlign: 'center'
     }
   }
@@ -54,27 +54,14 @@ function PartnerLogos({ classes }) {
         spacing={40}
         className={classes.mainGrid}
       >
-        <Grid item xs={12} sm={8} md={4}>
-          <Typography variant="h6" className={classes.titles}>
-            Our Partners
-          </Typography>
-        </Grid>
         <Grid
           container
-          xs={8}
-          spacing={24}
+          spacing={40}
           direction="row"
-          justify="space-evenly"
+          justify="center"
           alignItems="center"
           className={classes.partnerGrid}
         >
-          <Grid item xs={12} sm={4} md={4} lg={3}>
-            <img
-              src={data4sdg}
-              alt="World Bank Group"
-              className={classes.img}
-            />
-          </Grid>
           <Grid item xs={12} sm={4} md={4} lg={3}>
             <img
               src={worldbankgroup}
@@ -86,6 +73,13 @@ function PartnerLogos({ classes }) {
             <img
               src={liquidtelcom}
               alt="Liquid Telcom"
+              className={classes.img}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={3}>
+            <img
+              src={data4sdg}
+              alt="World Bank Group"
               className={classes.img}
             />
           </Grid>
