d605f04ba80bb92b23034042f98e26748fd88fbd
nerdlet <brndnyokabi@gmail.com>
2019-02-28 16:20:04 +0300

    Scale needle pointer

--- src/components/Header/JumbotronContent/AirCityHeaderContent/NeedlePointer.js
+++ src/components/Header/JumbotronContent/AirCityHeaderContent/NeedlePointer.js
@@ -10,20 +10,19 @@ class NeedlePointer extends Component {
     super(props);
     this.state = {};
   }
+
   render() {
-    const { classes } = this.props;
     return (
-      <svg className={classes.gaugeNeedleItem}>
-        <g transform="translate(450,300)">
-          <path
-            d="M5,0C3.333333333333333,-135,1.6666666666666667,-270,0,-270C-1.6666666666666667,-270,-3.333333333333333,0,-5,0C-3.333333333333333,0,-1.6666666666666667,5,0,5C1.6666666666666667,5,3.333333333333333,2.5,5,0"
-            strokeLinecap="round"
-            strokeWidth="5"
-            stroke="#144a3d"
-            style={{ cursor: 'grab' }}
-          />
-        </g>
-      </svg>
+      <g transform="translate(450,300)">
+        <path
+          transform="rotate(-64.75) scale(1.2)"
+          d="M5,0C3.333333333333333,-135,1.6666666666666667,-270,0,-270C-1.6666666666666667,-270,-3.333333333333333,0,-5,0C-3.333333333333333,0,-1.6666666666666667,5,0,5C1.6666666666666667,5,3.333333333333333,2.5,5,0"
+          strokeLinecap="round"
+          strokeWidth="5"
+          stroke="#144a3d"
+          style={{ cursor: 'grab' }}
+        />
+      </g>
     );
   }
 }
