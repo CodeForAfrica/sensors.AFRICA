82c81301ddd479e87d2bcb17aeee9172e11f7ff7
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-03-21 16:16:57 +0300

    initial

--- src/pages/air/Data.js
+++ src/pages/air/Data.js
@@ -27,6 +27,45 @@ function Data({ classes, url }) {
       <Navbar />
       <DataArchivesHeader />
       <DataArchives />
+      <div>
+        <h2>Share/Embed components with the following links:</h2>
+        <p>Sensor map https://sensors.africa/embeded/air/map?city=nairobi.</p>
+        <p>
+          Embed it in a page example:
+          <pre>{`
+            <iframe
+              src="https://sensors.africa/embeded/air/map?city=nairobi"
+              height="500px"
+              width="800px"
+            />
+          `}</pre>
+        </p>
+        <p>
+          Weekly data graph
+          https://sensors.africa/embeded/air/graph?city=nairobi.
+        </p>
+        <p>
+          Embed it in a page example:
+          <pre>{`
+            <iframe
+              src="https://sensors.africa/embeded/air/graph?city=nairobi"
+              height="500px"
+              width="800px"
+            />
+          `}</pre>
+        </p>
+        <p>The dial https://sensors.africa/embeded/air/dial?city=nairobi.</p>
+        <p>
+          Embed it in a page example:
+          <pre>{`
+            <iframe
+              src="https://sensors.africa/embeded/air/dial?city=nairobi"
+              height="500px"
+              width="800px"
+            />
+          `}</pre>
+        </p>
+      </div>
       <Stories />
       <Support classNames={classes.dataSupport} />
       <PartnerLogos />
