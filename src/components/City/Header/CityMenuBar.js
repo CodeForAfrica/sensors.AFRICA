44af2b3f625f600bdbceddaf727717d274ec566c
nerdlet <brndnyokabi@gmail.com>
2019-03-26 16:52:45 +0300

    Fix dial jumbotron height

--- src/components/City/Header/CityMenuBar.js
+++ src/components/City/Header/CityMenuBar.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import SearchBar from '../../SearchBar';
-import MenuBar from '../../Header/MenuBar';
 
 function CityMenuBar({ handleSearch }) {
   return (
-    <MenuBar>
-      <SearchBar
-        placeholder="Search for another location ..."
-        handleSearch={handleSearch}
-      />
-    </MenuBar>
+    <SearchBar
+      placeholder="Search for another location ..."
+      handleSearch={handleSearch}
+    />
   );
 }
 
