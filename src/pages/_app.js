0ab447214a37797e1416d6221067ad43b17bb4f1
nerdlet <brndnyokabi@gmail.com>
2020-03-11 14:13:28 +0300

    Add theme and custom app

---
+++ src/pages/_app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import App from 'next/app';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import 'assets/css/index.css';
+import 'assets/css/App.css';
+import theme from '../theme';
+
+export default class sensorsAfricaApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Index() {
+  return (
+    <div>
+      <p>This is the home page</p>
+    </div>
+  );
+}
