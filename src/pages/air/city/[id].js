b1e853656f04d3886ccd435190b98bf48afbe4ad
nerdlet <brndnyokabi@gmail.com>
2020-03-17 20:18:08 +0300

    Add city components

--- src/pages/air/city/[id].js
+++ src/pages/air/city/[id].js
@@ -1,10 +1,56 @@
 import React from 'react';
 
-import { useRouter } from 'next/router';
+import { useRouter, withRouter } from 'next/router';
+
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Navbar from 'components/Header/Navbar';
+import PartnerLogos from 'components/PartnerLogos';
+import Footer from 'components/Footer';
+import CallToAction from 'components/City/CallToAction';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+
+    // TODO(kilemensi): This is hack to force the page to be 100% wide w/o
+    //                  horizontal scrollbars.
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: '#fff'
+  }
+});
 
 function City() {
+  const classes = useStyles();
   const router = useRouter();
-  return <div>Example city: {router.query.id}</div>;
+  return (
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <Navbar />
+      </Grid>
+
+      <Grid item xs={12}>
+        <CallToAction />
+      </Grid>
+      <Grid item xs={12}>
+        This city is: {router.query.id}
+      </Grid>
+      <Grid item xs={12}>
+        <PartnerLogos />
+      </Grid>
+      <Grid item xs={12}>
+        <Footer />
+      </Grid>
+    </Grid>
+  );
 }
 
-export default City;
+export default withRouter(City);
