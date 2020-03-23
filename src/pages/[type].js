26a0b41ba21703e7c53080e8419c6c8d10d5faec
Clemence Kyara <clemence@codeforafrica.org>
2020-03-23 11:15:36 +0300

    Add /[types].js page to handle all coming soon pages

---
+++ src/pages/[type].js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import DocumentHead from 'components/DocumentHead';
+import { URLS } from 'components/DocumentHead/PageHeads';
+import Footer from 'components/Footer';
+import Hero from 'components/Landing/Hero';
+
+function SensorTypeHome({ type }) {
+  return (
+    <>
+      <DocumentHead url={URLS[type].HOME} />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+SensorTypeHome.propTypes = {
+  type: PropTypes.oneOf(['RADIATION', 'SOUND', 'WATER']).isRequired
+};
+
+export async function getStaticProps({ params: { type } }) {
+  return {
+    props: { type: type.toUpperCase() }
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { type: 'radiation' } },
+      { params: { type: 'sound' } },
+      { params: { type: 'water' } }
+    ],
+    fallback: false
+  };
+}
+
+export default SensorTypeHome;
