c5121cb903876e4d7885877fabc0d031a9c7804a
Clemence Kyara <clemence@codeforafrica.org>
2020-03-23 11:18:02 +0300

    Add DocumentHead support to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import Hero from 'components/Landing/Hero';
+import DocumentHead from 'components/DocumentHead';
+import { URLS } from 'components/DocumentHead/PageHeads';
 import Footer from 'components/Footer';
+import Hero from 'components/Landing/Hero';
 
 function Home() {
   return (
     <>
+      <DocumentHead url={URLS.HOME} />
       <Hero />
       <Footer />
     </>
