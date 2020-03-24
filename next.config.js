a4d95e54a80e17d76dd9695fed3fa9a2527a8943
nerdlet <brndnyokabi@gmail.com>
2020-03-24 13:46:27 +0300

    Remove css config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withCSS(
-  withImages({
-    webpack(config, { isServer }) {
-      // Important: return the modified config
+module.exports = withImages({
+  webpack(config, { isServer }) {
+    // Important: return the modified config
 
-      // https://github.com/jsoma/tabletop/issues/158
-      if (!isServer) {
-        // eslint-disable-next-line no-param-reassign
-        config.externals = ['tls', 'net', 'fs'];
-      }
-      return config;
+    // https://github.com/jsoma/tabletop/issues/158
+    if (!isServer) {
+      // eslint-disable-next-line no-param-reassign
+      config.externals = ['tls', 'net', 'fs'];
     }
-  })
-);
+    return config;
+  }
+});
