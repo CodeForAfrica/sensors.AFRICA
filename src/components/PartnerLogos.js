35907d3371f9fcdbe2c91ca971ab75efaa877c7d
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2022-12-14 16:05:48 +0300

    Revert "Addded Partner logos"
    This reverts commit 2b6065baffa54017dd6a753b0e766c5d13196721.
    
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- src/components/PartnerLogos.js
+++ src/components/PartnerLogos.js
@@ -2,10 +2,7 @@ import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 
-import car from "@/sensorsafrica/assets/images/partners/car.png";
-import godown from "@/sensorsafrica/assets/images/partners/godown.png";
 import liquidtelcom from "@/sensorsafrica/assets/images/partners/liquidtelcom.png";
-import msjc from "@/sensorsafrica/assets/images/partners/msjc.png";
 import data4sdg from "@/sensorsafrica/assets/images/partners/partnershipsdg.png";
 import worldbank from "@/sensorsafrica/assets/images/partners/worldbankgroup.png";
 import Figure from "@/sensorsafrica/components/Figure";
@@ -30,27 +27,12 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     padding: "2rem 0",
   },
-  carLogo: {
-    width: "100%",
-    textAlign: "center",
-    padding: "2rem 0",
-  },
   liquidLogo: {
     width: "100%",
     textAlign: "center",
     padding: "2rem 0",
   },
-  msjcLogo: {
-    width: "100%",
-    textAlign: "center",
-    padding: "2rem 0",
-  },
   globalLogo: {
-    width: "100%",
-    textAlign: "center",
-    paddingBottom: "2rem",
-  },
-  godownLogo: {
     width: "100%",
     textAlign: "center",
     paddingBottom: "2rem",
@@ -110,42 +92,6 @@ function PartnerLogos(props) {
             <Figure
               src={data4sdg}
               alt="Global Partnership for Sustainable Development Data"
-              className={classes.img}
-            />
-          </div>
-        </Grid>
-      </Grid>
-
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        className={classes.partnersLogo}
-      >
-        <Grid item xs={12} sm={4} md={3}>
-          <div className={classes.carLogo}>
-            <Figure
-              src={car}
-              alt="Center for Atmospherice Research"
-              className={classes.img}
-            />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <div className={classes.msjcLogo}>
-            <Figure
-              src={msjc}
-              alt="Mathare Socisal Justice Centre"
-              className={classes.img}
-            />
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <div className={classes.godownLogo}>
-            <Figure
-              src={godown}
-              alt="The Godown Arts Centre"
               className={classes.img}
             />
           </div>
