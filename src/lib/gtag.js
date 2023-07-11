f694044f715317e8151acd27309c6921f2af1d36
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-11 11:01:33 +0300

    Add GA

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "UA-44795600-44";
+export const GA_TRACKING_ID = "G-8M50XTMKVG";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
